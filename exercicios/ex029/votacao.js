6f71de2c24a37a83370eba6d2845d6977bda3359
GuilhermePierazo <pierazoemersonguilherme@gmail.com>
2024-09-04 18:31:46 -0300

    código de votação

--- exercicios/ex029/votacao.js
+++ exercicios/ex029/votacao.js
@@ -1,8 +1,11 @@
-var idade = 22
+var idade = 16
 if (idade < 16) {
     console.log('Não vota')
 } else if (idade < 18) {
 
     console.log('Voto opcional')
 }
- 
+
+else {
+    console.log(`Voto obrigatorio!`)
+}
